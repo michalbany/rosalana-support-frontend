f6aee7f09eeb98a75119e6d7647759fe269fff66
Michael Bany <mobany526@gmail.com>
2024-12-28 06:18:05 +0100

    test

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const { data } = await useApi<any>("/docs", {
+    method: "GET",
+});
+</script>
 <template>
-    Hello
+    <NuxtLayout name="main">
+        <pre class=" whitespace-pre-wrap">{{ data }}</pre>
+    </NuxtLayout>
 </template>
