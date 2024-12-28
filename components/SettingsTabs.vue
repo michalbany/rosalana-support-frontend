02550202980de6acb74900060e005e33db6c8c57
Michael Bany <mobany526@gmail.com>
2024-12-28 03:29:25 +0100

    form tabs responsive

--- components/SettingsTabs.vue
+++ components/SettingsTabs.vue
@@ -12,8 +12,16 @@
   );
 
   const defaultTab = computed(() => props.defaultTab || props.tabs[0]);
-
   const disabledTabs = ref<string[]>([]);
+  const activeTab = ref<string>(defaultTab.value);
+
+  const handleTabChanged = (tab: string | number) => {
+    activeTab.value = tab as string;
+  };
+
+  const changeTab = (tab: string) => {
+    activeTab.value = tab;
+  };
 
   const tabs = computed(() => {
     return props.tabs.map((tab) => {
@@ -33,8 +41,13 @@
     orientation="vertical"
     class="mt-6 flex w-full overflow-hidden"
     :default-value="defaultTab"
+    @update:model-value="handleTabChanged"
+    :model-value="activeTab"
   >
-    <UiTabsList class="flex h-full min-w-48 flex-col justify-start gap-1 rounded-lg" :pill="false">
+    <UiTabsList
+      class="hidden h-full min-w-48 flex-col justify-start gap-1 rounded-lg sm:flex"
+      :pill="false"
+    >
       <UiTabsTrigger
         class="w-full justify-start data-[state=active]:bg-zinc-200/80 data-[state=active]:text-primary dark:data-[state=active]:bg-zinc-800/80"
         :pill="false"
@@ -49,7 +62,32 @@
     </UiTabsList>
 
     <UiTabsContent :value="tab" v-for="(tab, i) in props.tabs" :key="i" class="w-full px-5">
-      <h3 class="text-2xl font-semibold">{{ textToLabel(tab) }}</h3>
+      <div class="flex items-center justify-between">
+        <h3 class="text-2xl font-semibold">{{ textToLabel(tab) }}</h3>
+        <UiDropdownMenu>
+          <UiDropdownMenuTrigger as-child>
+            <UiButton class="sm:hidden text-primary" size="icon" variant="ghost">
+              <Icon name="lucide:menu" class="h-4 w-4" />
+            </UiButton>
+          </UiDropdownMenuTrigger>
+          <UiDropdownMenuContent class="mr-8 w-36">
+            <UiDropdownMenuLabel :label="textToLabel(activeTab)" />
+            <UiDropdownMenuSeparator />
+            <UiDropdownMenuItem
+              v-for="(tab, i) in tabs"
+              :key="i"
+              :icon="props.tabsIcons[i]"
+              :disabled="disabledTabs.includes(tab)"
+              :title="textToLabel(tab)"
+              class="cursor-pointer"
+              @click="changeTab(tab)"
+              :class="{
+                'bg-zinc-200/80 text-primary dark:bg-zinc-800/80': activeTab === tab,
+              }"
+            />
+          </UiDropdownMenuContent>
+        </UiDropdownMenu>
+      </div>
       <UiDivider class="my-4" />
       <slot :name="tab" />
     </UiTabsContent>
